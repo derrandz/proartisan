b8c6246d920ed5356ca0c2d742a9caf7f210ede1
Kill <psycatham@gmail.com>
2015-08-13 17:36:37 +0100

    routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -9,7 +9,7 @@
  |------------------------------------------------------------
  */
 
-Route::GET('/proartisan/insert_commands', ['as' => 'proartisan.insert', 'uses' => 'ProArtisanController@InsertCommands']);
+Route::GET('/proartisan/insert_commands', ['as' => 'proartisan.insert', 'uses' => 'Hamzaouaghad\Proartisan\ProArtisanController@InsertCommands']);
 
 //To avoid timing out when running slow commands, we avoided to use POST.
-Route::GET('/proartisan/execute_commands', ['as' => 'proartisan.exec', 'uses' => 'ProArtisanController@ExecuteCommands']);
+Route::GET('/proartisan/execute_commands', ['as' => 'proartisan.exec', 'uses' => 'Hamzaouaghad\Proartisan\ProArtisanController@ExecuteCommands']);
